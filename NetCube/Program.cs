6e8de5c7ea4c918c634e38711433bb23ecdcd404
erdospeet11 <erdospepe9@gmail.com>
2025-11-27 00:41:46 +0200

    Trying to make a plane

--- NetCube/Program.cs
+++ NetCube/Program.cs
@@ -2,7 +2,11 @@
 using Silk.NET.Maths;
 using Silk.NET.Windowing;
 
-namespace MySilkProgram;
+//3D
+using Silk.NET.OpenGL;
+using System.Drawing;
+
+namespace Game;
 
 public class Program
 {
@@ -14,7 +18,7 @@ public class Program
         }
     }
 
-    private static void OnLoad() { 
+    private static unsafe void OnLoad() { 
         //Console.WriteLine("Load!");
 
         IInputContext input = _window.CreateInput();
@@ -22,7 +26,73 @@ public class Program
         for (int i = 0; i < input.Keyboards.Count; i++){
             input.Keyboards[i].KeyDown += KeyDown;
         }
-    }
+
+        _gl = _window.CreateOpenGL();
+
+        _gl.ClearColor(Color.CornflowerBlue);
+
+        _vao = _gl.GenVertexArray();
+
+        _gl.BindVertexArray(_vao);
+
+        float[] vertices = {
+            0.5f,  0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            -0.5f, -0.5f, 0.0f,
+            -0.5f,  0.5f, 0.0f
+        };
+
+        _vbo = _gl.GenBuffer();
+        _gl.BindBuffer(BufferTargetARB.ArrayBuffer, _vbo);
+
+        fixed (float* buf = vertices){
+            _gl.BufferData(BufferTargetARB.ArrayBuffer, (nuint) (vertices.Length * sizeof(float)), buf, BufferUsageARB.StaticDraw);
+        }
+
+        uint[] indices = {
+            0u, 1u, 3u,
+            1u, 2u, 3u
+        };
+
+        _ebo = _gl.GenBuffer();
+        _gl.BindBuffer(BufferTargetARB.ElementArrayBuffer, _ebo);
+
+        fixed (uint* buf = indices)
+        {
+            _gl.BufferData(BufferTargetARB.ElementArrayBuffer, (nuint) (indices.Length * sizeof(uint)), buf, BufferUsageARB.StaticDraw);
+        }
+
+        const string vertexCode = @"
+        #version 330 core
+
+        layout (location = 0) in vec3 aPosition;
+
+        void main()
+        {
+            gl_Position = vec4(aPosition, 1.0);
+        }";
+
+        const string fragmentCode = @"
+        #version 330 core
+
+        out vec4 out_color;
+
+        void main()
+        {
+            out_color = vec4(1.0, 0.5, 0.2, 1.0);
+        }";
+
+        uint vertexShader = _gl.CreateShader(ShaderType.VertexShader);
+        
+        _gl.ShaderSource(vertexShader, vertexCode);
+
+        _gl.CompileShader(vertexShader);
+
+        _gl.GetShader(vertexShader, ShaderParameterName.CompileStatus, out int vStatus);
+
+        if (vStatus != (int) GLEnum.True)
+            throw new Exception("Vertex shader failed to compile: " + _gl.GetShaderInfoLog(vertexShader));
+
 
     private static void OnUpdate(double deltaTime) { 
         //Console.WriteLine("Update!");
@@ -30,9 +100,15 @@ public class Program
 
     private static void OnRender(double deltaTime) { 
         //Console.WriteLine("Render!");
+        _gl.Clear(ClearBufferMask.ColorBufferBit);
     }
 
     private static IWindow _window;
+    private static GL _gl;
+    private static uint _vao;
+    private static uint _vbo;
+    private static uint _ebo;
+
     public static void Main(string[] args) { 
         WindowOptions options = WindowOptions.Default with
         {
---
+++ REAME.md
@@ -0,0 +1,11 @@
+Small test project to try out 3D rendering with Silk.NET.
+
+# Requirements
+- .NET 8.0
+- Silk.NET
+- Silk.NET.OpenGL
+- Silk.NET.Windowing
+
+# Running the project
+1. Clone the repository
+2. Run `dotnet run` in the root of the project
